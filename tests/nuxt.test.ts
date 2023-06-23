18c99a5f99eab5a105177027aea4e120748d2f5c
Lucas Garcia <lsglucas@protonmail.com>
2023-06-22 23:58:44 -0300

    feat(test): add vitest and example

---
+++ tests/nuxt.test.ts
@@ -0,0 +1,9 @@
+import { expect, describe, test } from "vitest";
+
+describe("Test Nuxt Example", () => {
+  test("example", () => {
+    const example = "example";
+
+    expect(example).toBe("example");
+  });
+});
