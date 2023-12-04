db36a6372ed0bb763b4542e9d2ee586e209e85d5
lsglucas <lsglucas@protonmail.com>
2023-12-04 04:32:10 -0300

    chore: upgraded @nuxt/i18n

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -36,8 +36,9 @@ export default defineNuxtConfig({
 
   // i18n
   i18n: {
+    baseUrl: process.env.NUXT_PUBLIC_SITE_URL,
     langDir: "lang",
-    defaultLocale: "en",
+    defaultLocale: "pt",
     strategy: "prefix_and_default",
     types: "composition",
     locales: [
@@ -52,6 +53,11 @@ export default defineNuxtConfig({
         file: "pt-BR.ts",
       },
     ],
+    detectBrowserLanguage: {
+      useCookie: true,
+      cookieKey: "i18n_redirected",
+      redirectOn: "root",
+    },
   },
 
   // google fonts
